8590df437b2a8985d23d92d1854d3fad082caea9
mamun <mdmamunriyan361@gmail.com>
2025-10-06 13:16:06 +0600

    dfa

--- app.js
+++ app.js
@@ -9,5 +9,11 @@ if(vc == "a" || vc =="e" || vc=="i" || vc=="o" || vc=="u")
 else if(vc<=0 || vc>100)
     document.writeln("invalid");
 
+else if(vc == "b" || vc =="c" || vc=="d" || vc=="f" || vc=="g" || vc == "h"
+     || vc =="m" || vc=="j" || vc=="k" || vc=="l" 
+    || vc == "n" || vc =="p" || vc=="q" || vc=="r" || vc=="s" ||vc == "t" 
+    || vc =="V" || vc=="w" || vc=="x" || vc=="Y" || vc=="z")
+    document.writeln("consonant")
+
 else
-document.writeln("consonant");
+document.writeln("invalids");
