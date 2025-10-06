e40d4b194c88d21cc7d5d5eb135c5402b4bbf981
mamun <mdmamunriyan361@gmail.com>
2025-10-06 13:25:19 +0600

    fja

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ let vc=prompt("Enter your Letter");
 
 vc=vc.toLowerCase();
 
+
 if(vc == "a" || vc =="e" || vc=="i" || vc=="o" || vc=="u")
-    document.writeln("vowel");
+    document.writeln("vowel = " + vc);
 
 else if(vc<=0 || vc>100)
     document.writeln("invalid");
@@ -13,7 +14,7 @@ else if(vc == "b" || vc =="c" || vc=="d" || vc=="f" || vc=="g" || vc == "h"
      || vc =="m" || vc=="j" || vc=="k" || vc=="l" 
     || vc == "n" || vc =="p" || vc=="q" || vc=="r" || vc=="s" ||vc == "t" 
     || vc =="V" || vc=="w" || vc=="x" || vc=="Y" || vc=="z")
-    document.writeln("consonant")
+    document.writeln("consonant = " + vc)
 
 else
 document.writeln("invalids");
