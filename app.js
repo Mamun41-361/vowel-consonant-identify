c7913ab87d5fd0dadf9a9e57c5ae1753f10465c5
mamun <mdmamunriyan361@gmail.com>
2025-10-06 13:05:32 +0600

    df

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 
+let vc=prompt("Enter your Letter");
+
+vc=vc.toLowerCase();
+
+if(vc == "a" || vc =="e" || vc=="i" || vc=="o" || vc=="u")
+    document.writeln("vowel");
+
+else if(vc<=0 || vc>100)
+    document.writeln("invalid");
+
+else
+document.writeln("consonant");
